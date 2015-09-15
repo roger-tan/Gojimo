3f8c9f6cb17f9ce553aa5bbc7f0b6a45d1f52ce6
Roger TAN <mail+github@rogertan.fr>
2015-09-14 20:59:50 -0700

    First initial extension

---
+++ Gojimo/Classes/Models/Extensions/Qualification+Request.swift
@@ -0,0 +1,13 @@
+//
+//  Qualification+Request.swift
+//  Gojimo
+//
+//  Created by Roger TAN on 14/09/15.
+//  Copyright © 2015 Roger TAN. All rights reserved.
+//
+
+import Foundation
+
+extension Qualification {
+
+}
