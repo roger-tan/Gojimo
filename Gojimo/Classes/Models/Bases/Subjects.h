2ff63168c2f9a370dbd1e0e6a159cd9ea4e736b1
Roger TAN <mail+github@rogertan.fr>
2015-09-15 21:51:39 -0700

    Updated Copyright

--- Gojimo/Classes/Models/Bases/Subjects.h
+++ Gojimo/Classes/Models/Bases/Subjects.h
@@ -2,7 +2,7 @@
 //  Subjects.h
 //
 //  Created by Roger TAN on 14/09/15
-//  Copyright (c) 2015 __MyCompanyName__. All rights reserved.
+//  Copyright (c) 2015 Roger TAN. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
