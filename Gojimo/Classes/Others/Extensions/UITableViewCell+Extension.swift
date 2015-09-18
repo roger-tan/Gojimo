cdde409ec20de67ee8fd486bba3384cfe2754007
Roger TAN <mail+github@rogertan.fr>
2015-09-18 09:09:31 -0700

    Create an extension of UITableViewCell

---
+++ Gojimo/Classes/Others/Extensions/UITableViewCell+Extension.swift
@@ -0,0 +1,18 @@
+//
+//  UITableViewCell+Extension.swift
+//  Gojimo
+//
+//  Created by Roger TAN on 18/09/15.
+//  Copyright © 2015 Roger TAN. All rights reserved.
+//
+
+import UIKit
+
+extension UITableViewCell {
+
+    class internal func nib() -> UINib {
+        let nameSpaceClassName = NSStringFromClass(self)
+        let className = nameSpaceClassName.componentsSeparatedByString(".").last! as String
+        return UINib(nibName: className, bundle: nil)
+    }
+}
